e9b9f2335a272b8cba3a2a8ad884262adf57c450
BulatXam <Khamdbulat@yandex.ru>
2021-12-27 23:53:24 +0300

    feat: add manager for execute from command line

---
+++ telegSpam/manage.py
@@ -0,0 +1,12 @@
+import sys
+
+import asyncio
+
+from console_managment.base import execute_from_command_line
+
+if __name__ == "__main__":
+    # asyncio.set_event_loop(asyncio.SelectorEventLoop())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(execute_from_command_line(sys.argv))
+
+# Ruler 80
