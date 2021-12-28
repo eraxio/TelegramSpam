82ed288e4dafce1c84946d12af8ed17b336e110a
BulatXam <Khamdbulat@yandex.ru>
2021-12-28 22:04:23 +0300

    refactor: refactor loop asyncio

--- telegSpam/manage.py
+++ telegSpam/manage.py
@@ -5,7 +5,7 @@ import asyncio
 from console_managment.base import execute_from_command_line
 
 if __name__ == "__main__":
-    # asyncio.set_event_loop(asyncio.SelectorEventLoop())
+    asyncio.set_event_loop(asyncio.SelectorEventLoop())
     loop = asyncio.get_event_loop()
     loop.run_until_complete(execute_from_command_line(sys.argv))
 
